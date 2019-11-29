03ca3368fbf0a8ff071936a86991d026e8b64356
hs <1002124115@qq.com>
2019-11-29 23:45:58 +0800

    把本低工程添加到git

---
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,17 @@
+package com.itheima.git;
+
+/*** 
+ * @Auther: hs
+ * @Date: 2019/11/29 23:29 
+ *
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+
+    }
+}
